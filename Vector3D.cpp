cce5f1f3737a81bb3f6b3e66c5a2d2baffb8d5be
Fernando Éricles <fernandoericles98@gmail.com>
2023-09-23 23:28:25 -0300

    update vector3d default constructor

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -1,7 +1,6 @@
 #include "Vector3D.h"
 
-Vector3D::Vector3D() : x(0.0f), y(0.0f), z(0.0f) {
-}
+Vector3D::Vector3D() = default;
 
 Vector3D::Vector3D(float x, float y, float z) : x(x), y(y), z(z) {
 }
